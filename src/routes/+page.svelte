499606419004538a9cf876f2aa927c89466a6925
notgabie <gabriellamondvay@gmail.com>
2025-05-11 23:07:03 +0200

    :sparkles: feat: add basic styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,5 +12,7 @@ function onClick() {
 
 <h1 class="text-3xl">Art prompt generator</h1>
 <p>Click the button to generate a random art prompt.</p>
-<button onclick={onClick} class="border-1 p-2 rounded-md cursor-pointer">Generate</button>
-<p>{prompt}</p>
+<div class="flex flex-col gap-2 p-2 border-1 rounded-md h-[200px] w-2xl items-center justify-around">
+    <button onclick={onClick} class="border-1 p-2 rounded-md cursor-pointer">Generate</button>
+    <p class="">{prompt}</p>
+</div>
